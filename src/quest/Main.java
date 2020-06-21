7a352c1d04d138e8150563e1c00d94922e90302a
HHS-Matt <19104588@student.hhs.nl>
2020-06-21 18:03:36 +0200

    Deleted getRandom() & changed randomizer for creating comets

--- src/quest/Main.java
+++ src/quest/Main.java
@@ -109,16 +109,19 @@ public class Main extends Application {
 
     //creates the game, includes creating the board with tiles, meteorites & the player.
     private Parent createGame() {
+        //create game screen.
         StackPane root = new StackPane();
         root.setPrefSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         game = new Pane();
         game.setPrefSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+        //create score.
         String player_score = "Score: " + score;
         score_text = new Label(player_score);
         score_text.setFont(new Font(38));
         StackPane.setAlignment(score_text, Pos.TOP_CENTER);
 
+        //create tiles with background + setting the neighbours of tiles.
         Image tile_background = new Image (new File(resources_path + "space-background.png").toURI().toString());
 
         for (int y = 0; y < Y_TILES; y++) {
@@ -134,19 +137,21 @@ public class Main extends Application {
             }
         }
 
+        //create player.
         user = new User(new Image (new File(resources_path + "spaceship.png").toURI().toString()), grid[0][0], "up");
         grid[0][0].setObject(user);
 
         //creating Comets.
         for(int i = 0; i < comets.length; i++)
         {
-            int posX = getRandom(1, X_TILES -1);
-            int posY = getRandom(1, Y_TILES -1);
+            int posX = ThreadLocalRandom.current().nextInt(1, X_TILES);
+            int posY = ThreadLocalRandom.current().nextInt(1, X_TILES);
             Comet comet = new Comet(new Image (new File(resources_path + "Meteorites.png").toURI().toString()), grid[posX][posY]);
             grid[posX][posY].setObject(comet);
             comets[i] = comet;
         }
 
+        //creating planets
         this.planet_count = 10; // TODO ? Move to game config object.
         this.planets = new Planet[this.planet_count];
         for (Planet planet : this.planets) {
@@ -161,9 +166,10 @@ public class Main extends Application {
             planet_tile.setObject(planet);
         }
 
+        //updating game.
         updateGame();
 
-
+        //set all game objects in scene.
         root.getChildren().addAll(game, score_text);
         return root;
     }
@@ -214,12 +220,6 @@ public class Main extends Application {
         }
 
         return neighbors;
-    }
-
-    //get a random number between a min & a max.
-    private int getRandom(int min, int max)   {
-        int temp = (int)(Math.random() * (max - min + 1) + min);
-        return temp;
     }
 
     //update & Render
