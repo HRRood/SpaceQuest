3dad7bedc854f21ab4ca715c69d07f3804eb10d6
Yunus Emre Kurtal <yekurtal@icloud.com>
2020-06-21 12:08:07 +0200

    Removed forgotten test variable.

--- src/quest/Planet.java
+++ src/quest/Planet.java
@@ -5,7 +5,6 @@ import javafx.scene.image.Image;
 public class Planet extends Object {
 
     private final Image visited_sprite;
-    public final String test = "testing123...";
 
     public Planet(Image unvisited_sprite, Image visited_sprite, Tile tile) {
         super(unvisited_sprite, tile);
