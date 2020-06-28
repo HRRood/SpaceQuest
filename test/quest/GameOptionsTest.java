c002e49b3ac199616e4a7d5de3bd9dd5a3391783
Yunus Emre Kurtal <yekurtal@icloud.com>
2020-06-28 17:28:09 +0200

    Added game options and grid neighbour tests and added .DS_Store to gitignore.

---
+++ test/quest/GameOptionsTest.java
@@ -0,0 +1,27 @@
+package quest;
+
+import org.junit.Test;
+
+public class GameOptionsTest {
+
+    @Test
+    public void XCountEqualsGameOption() {
+        //
+    }
+
+    @Test
+    public void YCountEqualsGameOption() {
+        //
+    }
+
+    @Test
+    public void PlanetCountEqualsGameOption() {
+        //
+    }
+
+    @Test
+    public void CometCountEqualsGameOption() {
+        //
+    }
+
+}
---
+++ test/quest/GridNeighboursTest.java
@@ -0,0 +1,27 @@
+package quest;
+
+import org.junit.Test;
+
+public class GridNeighboursTest {
+
+    @Test
+    public void upperNeighbourValid() {
+        //
+    }
+
+    @Test
+    public void rightNeighbourValid() {
+        //
+    }
+
+    @Test
+    public void bottomNeighbourValid() {
+        //
+    }
+
+    @Test
+    public void leftNeighbourValid() {
+        //
+    }
+
+}
