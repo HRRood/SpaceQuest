27093f65e16efb34ff28b849e3a17a4add096659
Roy Roodenburg <royroodtje@gmail.com>
2020-06-28 19:53:38 +0200

    removed warnings

--- test/quest/MovementTest.java
+++ test/quest/MovementTest.java
@@ -2,41 +2,36 @@ package quest;
 
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MovementTest {
     @Test
     public void moveDownTest() throws InterruptedException {
         final int[] pos_x = new int[1];
         final int[] pos_y = new int[1];
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread(() -> {
+            new JFXPanel(); // Initializes the JavaFx Platform
+            Platform.runLater(new Runnable() {
 
-            @Override
-            public void run() {
-                new JFXPanel(); // Initializes the JavaFx Platform
-                Platform.runLater(new Runnable() {
-
-                    Main m;
-                    @Override
-                    public void run() {
-                        try {
-                            m = new Main();
-                            m.start(new Stage());
-                            m.gotoGame();
-                            m.game.user.handleKeyPressed(KeyCode.DOWN);
-                            pos_x[0] = m.game.user.getTile().getPosition_x();
-                            pos_y[0] = m.game.user.getTile().getPosition_y();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                Main m;
+                @Override
+                public void run() {
+                    try {
+                        m = new Main();
+                        m.start(new Stage());
+                        m.gotoGame();
+                        m.game.user.handleKeyPressed(KeyCode.DOWN);
+                        pos_x[0] = m.game.user.getTile().getPosition_x();
+                        pos_y[0] = m.game.user.getTile().getPosition_y();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-            }
+                }
+            });
         });
         thread.start();// Initialize the thread
         Thread.sleep(3000); // Time to use the app, with out this, the thread
@@ -49,29 +44,25 @@ public class MovementTest {
     public void moveLeftTest() throws InterruptedException {
         final int[] pos_x = new int[1];
         final int[] pos_y = new int[1];
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread(() -> {
+            new JFXPanel(); // Initializes the JavaFx Platform
+            Platform.runLater(new Runnable() {
 
-            @Override
-            public void run() {
-                new JFXPanel(); // Initializes the JavaFx Platform
-                Platform.runLater(new Runnable() {
-
-                    Main m;
-                    @Override
-                    public void run() {
-                        try {
-                            m = new Main();
-                            m.start(new Stage());
-                            m.gotoGame();
-                            m.game.user.handleKeyPressed(KeyCode.LEFT);
-                            pos_x[0] = m.game.user.getTile().getPosition_x();
-                            pos_y[0] = m.game.user.getTile().getPosition_y();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                Main m;
+                @Override
+                public void run() {
+                    try {
+                        m = new Main();
+                        m.start(new Stage());
+                        m.gotoGame();
+                        m.game.user.handleKeyPressed(KeyCode.LEFT);
+                        pos_x[0] = m.game.user.getTile().getPosition_x();
+                        pos_y[0] = m.game.user.getTile().getPosition_y();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-            }
+                }
+            });
         });
         thread.start();// Initialize the thread
         Thread.sleep(3000); // Time to use the app, with out this, the thread
@@ -84,29 +75,25 @@ public class MovementTest {
     public void moveUpTest() throws InterruptedException {
         final int[] pos_x = new int[1];
         final int[] pos_y = new int[1];
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread(() -> {
+            new JFXPanel(); // Initializes the JavaFx Platform
+            Platform.runLater(new Runnable() {
 
-            @Override
-            public void run() {
-                new JFXPanel(); // Initializes the JavaFx Platform
-                Platform.runLater(new Runnable() {
-
-                    Main m;
-                    @Override
-                    public void run() {
-                        try {
-                            m = new Main();
-                            m.start(new Stage());
-                            m.gotoGame();
-                            m.game.user.handleKeyPressed(KeyCode.UP);
-                            pos_x[0] = m.game.user.getTile().getPosition_x();
-                            pos_y[0] = m.game.user.getTile().getPosition_y();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                Main m;
+                @Override
+                public void run() {
+                    try {
+                        m = new Main();
+                        m.start(new Stage());
+                        m.gotoGame();
+                        m.game.user.handleKeyPressed(KeyCode.UP);
+                        pos_x[0] = m.game.user.getTile().getPosition_x();
+                        pos_y[0] = m.game.user.getTile().getPosition_y();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-            }
+                }
+            });
         });
         thread.start();// Initialize the thread
         Thread.sleep(3000); // Time to use the app, with out this, the thread
@@ -119,29 +106,25 @@ public class MovementTest {
     public void moveRightTest() throws InterruptedException {
         final int[] pos_x = new int[1];
         final int[] pos_y = new int[1];
-        Thread thread = new Thread(new Runnable() {
+        Thread thread = new Thread(() -> {
+            new JFXPanel(); // Initializes the JavaFx Platform
+            Platform.runLater(new Runnable() {
 
-            @Override
-            public void run() {
-                new JFXPanel(); // Initializes the JavaFx Platform
-                Platform.runLater(new Runnable() {
-
-                    Main m;
-                    @Override
-                    public void run() {
-                        try {
-                            m = new Main();
-                            m.start(new Stage());
-                            m.gotoGame();
-                            m.game.user.handleKeyPressed(KeyCode.RIGHT);
-                            pos_x[0] = m.game.user.getTile().getPosition_x();
-                            pos_y[0] = m.game.user.getTile().getPosition_y();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                Main m;
+                @Override
+                public void run() {
+                    try {
+                        m = new Main();
+                        m.start(new Stage());
+                        m.gotoGame();
+                        m.game.user.handleKeyPressed(KeyCode.RIGHT);
+                        pos_x[0] = m.game.user.getTile().getPosition_x();
+                        pos_y[0] = m.game.user.getTile().getPosition_y();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-            }
+                }
+            });
         });
         thread.start();// Initialize the thread
         Thread.sleep(3000); // Time to use the app, with out this, the thread
